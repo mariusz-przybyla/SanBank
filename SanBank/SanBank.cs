7084607af6eb4c81b64870b394130bba31effbad
mariusz-przybyla <mariusz.przybyla8@wp.pl>
2019-11-30 12:10:42 +0100

    First commit-test

---
+++ SanBank/SanBank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SanBank.method;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            View view = new View();
+
+            view.ViewMenu();
+           
+  
+
+            
+        }
+    }
+}
---
+++ SanBank/method/View.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanBank.method
+{
+    class View
+    {
+        public void ViewMenu() {
+            Console.WriteLine(" ___________________________");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("|          SanBank          |");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("| 1. Stwórz konto           |");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("| 2. Obsługa konta          |");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("| 3. Informacje o koncie    |");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("| 4. Usuń konto             |");
+            Console.WriteLine("|___________________________|");
+        }
+
+    }
+}
